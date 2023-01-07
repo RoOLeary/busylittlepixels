229fa49124b2d07f9a67ddc0b4778ac28fa3b5da
Ronan <roleary81@gmail.com>
2023-01-07 19:51:06 +0100

    fix error on link

--- pages/words/[slug].tsx
+++ pages/words/[slug].tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/jsx-no-comment-textnodes */
 import Head from 'next/head'
 import Image from 'next/image'
-import Link from 'next/Link'
+import Link from 'next/link'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 
