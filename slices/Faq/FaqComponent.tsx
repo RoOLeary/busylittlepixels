a987b4387c776f78af2d4123ed4746e00de6e3f2
Ronan <roleary81@gmail.com>
2023-01-14 13:34:17 +0100

    add studio to nav

--- slices/Faq/FaqComponent.tsx
+++ slices/Faq/FaqComponent.tsx
@@ -10,7 +10,7 @@ export const FaqComponent = (props: FaqProps) => {
     const [expanded, setExpanded] = useState(false);
     return <div onClick={() => setExpanded(!expanded)} className="flex flex-col pb-6 border-b last:border-b-0 text-gray-light cursor-pointer">
         <div className="flex flex-row items-center">
-            <p className={`flex-auto active:text-red-600 hover:text-red-600 py-4 ${expanded ? "text-red-600 font-black" : "font-black"} uppercase`}>{props.question}</p>
+            <p className={`flex-auto active:text-red-500 hover:text-red-500 py-4 easing-linear ${expanded ? "text-red-500 font-black" : "font-black"} uppercase`}>{props.question}</p>
         </div>
         <div className={`transition-max-height duration-700 ease-in-out overflow-hidden ${expanded ? "max-h-20" : "max-h-0"}`}>
             <p>{props.answer}</p>
