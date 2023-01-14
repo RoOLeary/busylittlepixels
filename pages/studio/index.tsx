c57f71a664e0bb9941223f4cabbe0b16a0a397ff
Ronan <roleary81@gmail.com>
2023-01-14 20:48:04 +0100

    update index

--- pages/studio/index.tsx
+++ pages/studio/index.tsx
@@ -23,7 +23,7 @@ export default function Studio() {
     return(
 
         <Layout>
-            {/* <div className="isolate bg-white">
+            <section className="isolate bg-white">
                 <div className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]">
                     <svg className="relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]" viewBox="0 0 1155 678" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path fill="url(#45de2b6b-92d5-4d68-a6a0-9b9b2abad533)" fillOpacity=".3" d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z" />
@@ -35,32 +35,7 @@ export default function Studio() {
                         </defs>
                     </svg>
                 </div>
-
-                <main>
-                    <div className="relative px-6 lg:px-8">
-                        <div className="mx-auto max-w-4xl pt-20 pb-2 sm:pt-48 md:pb-20">
-                            <div>
-                               
-                                <div>
-                                    <h1 className="text-4xl font-black tracking-tightest sm:text-center sm:text-6xl uppercase">busy <span style={{ "color": "red" }}>little</span> pixels<span style={{ "color": "red" }}>.</span><span style={{ "color": "red" }}>STUDIO</span></h1>
-                                    <p className="mt-6 text-lg leading-8 text-gray-600 text-left md:text-center">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
-                                </div>
-                                <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
-                                    <svg className="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]" viewBox="0 0 1155 678" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path fill="url(#ecb5b0c9-546c-4772-8c71-4d3f06d544bc)" fillOpacity=".3" d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z" />
-                                        <defs>
-                                            <linearGradient id="ecb5b0c9-546c-4772-8c71-4d3f06d544bc" x1="1155.49" x2="-78.208" y1=".177" y2="474.645" gradientUnits="userSpaceOnUse">
-                                                <stop stopColor="#9089FC"></stop>
-                                                <stop offset="1" stopColor="#FF80B5"></stop>
-                                            </linearGradient>
-                                        </defs>
-                                    </svg>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </main>
-            </div> */}
+            </section>
 
 
 
