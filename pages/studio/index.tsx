56412d4a16b3221ce50e176dec749522ea40cb74
Ronan <roleary81@gmail.com>
2023-01-16 01:20:15 +0100

    add another component to studio

--- pages/studio/index.tsx
+++ pages/studio/index.tsx
@@ -82,7 +82,51 @@ export default function Studio() {
                     </div>
                 </div>
             </Bounded>
-           
+            <section className="text-gray-600 body-font">
+                <div className="container px-5 py-24 mx-auto">
+                    <div className="flex flex-wrap w-full mb-20">
+                    <div className="lg:w-1/2 w-full mb-6 lg:mb-0">
+                        <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Pitchfork Kickstarter Taxidermy</h1>
+                        <div className="h-1 w-20 bg-red-500 rounded"></div>
+                    </div>
+                    <p className="lg:w-1/2 w-full leading-relaxed text-gray-500">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep jianbing selfies heirloom prism food truck ugh squid celiac humblebrag.</p>
+                    </div>
+                    <div className="flex flex-wrap -m-4">
+                    <div className="xl:w-1/4 md:w-1/2 p-2">
+                        <div className="bg-gray-100 p-6 rounded-lg">
+                        {/* <img className="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/720x400" alt="content" /> */}
+                        <h3 className="tracking-widest text-red-500 text-xs font-medium title-font">SUBTITLE</h3>
+                        <h2 className="text-lg text-gray-900 font-medium title-font mb-4">Chichen Itza</h2>
+                        <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+                        </div>
+                    </div>
+                    <div className="xl:w-1/4 md:w-1/2 p-2">
+                        <div className="bg-gray-100 p-6 rounded-lg">
+                        {/* <img className="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/721x401" alt="content" /> */}
+                        <h3 className="tracking-widest text-red-500 text-xs font-medium title-font">SUBTITLE</h3>
+                        <h2 className="text-lg text-gray-900 font-medium title-font mb-4">Colosseum Roma</h2>
+                        <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+                        </div>
+                    </div>
+                    <div className="xl:w-1/4 md:w-1/2 p-2">
+                        <div className="bg-gray-100 p-6 rounded-lg">
+                        {/* <img className="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/722x402" alt="content" /> */}
+                        <h3 className="tracking-widest text-red-500 text-xs font-medium title-font">SUBTITLE</h3>
+                        <h2 className="text-lg text-gray-900 font-medium title-font mb-4">Great Pyramid of Giza</h2>
+                        <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+                        </div>
+                    </div>
+                    <div className="xl:w-1/4 md:w-1/2 p-2">
+                        <div className="bg-gray-100 p-6 rounded-lg">
+                        {/* <img className="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/723x403" alt="content" /> */}
+                        <h3 className="tracking-widest text-red-500 text-xs font-medium title-font">SUBTITLE</h3>
+                        <h2 className="text-lg text-gray-900 font-medium title-font mb-4">San Francisco</h2>
+                        <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </section>
             <div className="bg-white">
                 <div className="mx-auto grid max-w-2xl grid-cols-1 items-center gap-y-16 gap-x-8 py-24 px-4 sm:px-6 sm:py-32 lg:max-w-7xl lg:grid-cols-2 lg:px-8">
                     <div>
