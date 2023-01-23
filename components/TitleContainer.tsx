37e03266356d7e857a8bdc0a30fbc2e195e58926
Ronan <roleary81@gmail.com>
2023-01-23 08:15:58 +0100

    title change

--- components/TitleContainer.tsx
+++ components/TitleContainer.tsx
@@ -6,7 +6,7 @@ export const TitleContainer = () => {
         <div className="title_container">
             <div className="grid grid-cols-1 justify-items-center gap-10 homeAdjust mb-6">
                 <div className="mx-auto w-full text-start mt-4">
-                    <h1 className="px-6 font-black text-4xl md:text-6xl mb-6 last:mb-0 uppercase">WE ARE busy<span style={{ "color": "red" }}>little</span>pixels<span style={{ "color": "red" }}>.</span><br/> WE <span style={{ "color": "red" }}>HEART </span> {word}</h1>
+                    <h1 className="px-6 font-black text-4xl md:text-6xl mb-6 last:mb-0 uppercase">WE ARE busy<span style={{ "color": "red" }}>little</span>pixels<br/>& WE <span style={{ "color": "red" }}>HEART </span> {word}</h1>
                 </div>
             </div>
         </div>
