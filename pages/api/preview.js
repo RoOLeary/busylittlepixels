483b7b893cade7b11ea91e31a5000961d5617b2c
Ronan <roleary81@gmail.com>
2023-01-24 21:04:13 +0100

    dgdfgd

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -1,22 +1,17 @@
 export default async (req, res) => {
-   
-  if (req.query.token === null || !req.query.slug) {
+  if (req.query.token === null) {
     return res.status(401).json({ message: 'No preview token' })
   }
   if (req.query.uri === null) {
     return res.status(401).json({ message: 'No URI provided' })
   }
-  
   res.setPreviewData(
     {
       token: req.query.token ?? null,
     },
     {
-      title: req.query.uri
-    },
-    {
       maxAge: 60,
     }
   )
-  res.redirect(`/${req.query.slug}`)
+  res.redirect(`/${req.query.uri}`)
 }
