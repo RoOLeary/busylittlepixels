c61b061c1f2692af92b25032c396172330c01d00
Ronan <roleary81@gmail.com>
2023-01-25 19:34:00 +0100

    add props to container

--- components/TitleContainer.tsx
+++ components/TitleContainer.tsx
@@ -1,7 +1,12 @@
 import { useTypingText } from '../hooks/useTypingText';
 
 
-export const TitleContainer = () => {
+interface ITitle {
+    title?: String,
+    subtitle?: String
+}
+
+export const TitleContainer = ({ title, subtitle }:ITitle) => {
 
    
     const { word } = useTypingText(['CODE...', 'APPS...', 'TECH...', 'DESIGN...', 'GAMES...', 'PUPPIES...', 'MATÉ...'], 250, 20);
@@ -10,7 +15,7 @@ export const TitleContainer = () => {
         <div className="title_container">
             <div className="grid grid-cols-1 justify-items-center gap-10 homeAdjust mb-6">
                 <div className="mx-auto w-full text-start mt-4">
-                    <h1 className="px-6 font-black text-4xl md:text-6xl mb-6 last:mb-0 uppercase">WE ARE BUSY<span style={{ "color": "red" }}>LITTLE</span>PIXELS<span style={{ "color": "red" }}>.</span><br/> WE <span style={{ "color": "red" }}>HEART </span> <span style={{ "color": "red" }}>{word}</span></h1>
+                    <h1 className="px-6 font-black text-4xl md:text-6xl mb-6 last:mb-0 uppercase">WE ARE BUSY<span style={{ "color": "red" }}>{title}</span>PIXELS<span style={{ "color": "red" }}>.</span><br/>{subtitle} <span style={{ "color": "red" }}>{word}</span></h1>
                 </div>
             </div>
         </div>
