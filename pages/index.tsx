db565beb2c434bbee479693d4f32fae0b0af95e5
Ronan <roleary81@gmail.com>
2023-01-25 22:04:30 +0100

    updates document to fix SEO errors

--- pages/index.tsx
+++ pages/index.tsx
@@ -88,41 +88,39 @@ const Home = ({ page, preview }:any) => {
             </div>
             <div>
               <div className="mt-10">
-                
                 <div aria-hidden="true" className="pointer-events-none lg:absolute lg:inset-y-0 lg:mx-auto lg:w-full lg:max-w-7xl">
                   <div className="absolute transform sm:left-1/2 sm:top-0 sm:translate-x-8 lg:left-1/2 lg:top-1/2 lg:-translate-y-1/2 lg:translate-x-8">
-                    <div className="flex items-center space-x-6 lg:space-x-8">
-                      <div className="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
-                        <div className="h-64 w-44 overflow-hidden rounded-lg sm:opacity-0 lg:opacity-100">
-                          <img src="https://placedog.net/200/375/r" alt="" className="h-full w-full object-cover object-center" />
-                        </div>
-                        <div className="h-64 w-44 overflow-hidden rounded-lg">
-                          <img src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-02.jpg" alt="" className="h-full w-full object-cover object-center" />
-                        </div>
+                      <div className="flex items-center space-x-6 lg:space-x-8">
+                          <div className="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
+                              <div className="h-64 w-44 overflow-hidden rounded-lg sm:opacity-0 lg:opacity-100">
+                                  <img src="https://placedog.net/200/375/r" alt="" className="h-full w-full object-cover object-center" />
+                              </div>
+                              <div className="h-64 w-44 overflow-hidden rounded-lg">
+                                  <img src="https://placedog.net/350/500/r" alt="" className="h-full w-full object-cover object-center" />
+                              </div>
+                          </div>
+                          <div className="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
+                              <div className="h-64 w-44 overflow-hidden rounded-lg">
+                                  <img src="https://placedog.net/200/375/r" alt="" className="h-full w-full object-cover object-center" />
+                              </div>
+                              <div className="h-64 w-44 overflow-hidden rounded-lg">
+                                  <img src="https://placedog.net/352/512/r" alt="" className="h-full w-full object-cover object-center" />
+                              </div>
+                              <div className="h-64 w-44 overflow-hidden rounded-lg">
+                                  <img src="https://placedog.net/200/375/r" alt="" className="h-full w-full object-cover object-center" />
+                              </div>
+                          </div>
+                          <div className="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
+                              <div className="h-64 w-44 overflow-hidden rounded-lg">
+                                  <img src="https://placedog.net/352/512/r" alt="" className="h-full w-full object-cover object-center" />
+                              </div>
+                              <div className="h-64 w-44 overflow-hidden rounded-lg">
+                                  <img src="https://placedog.net/200/375/r" alt="" className="h-full w-full object-cover object-center" />
+                              </div>
+                          </div>
                       </div>
-                      <div className="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
-                        <div className="h-64 w-44 overflow-hidden rounded-lg">
-                          <img src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-03.jpg" alt="" className="h-full w-full object-cover object-center" />
-                        </div>
-                        <div className="h-64 w-44 overflow-hidden rounded-lg">
-                          <img src="https://placedog.net/352/512/r" alt="" className="h-full w-full object-cover object-center" />
-                        </div>
-                        <div className="h-64 w-44 overflow-hidden rounded-lg">
-                          <img src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-05.jpg" alt="" className="h-full w-full object-cover object-center" />
-                        </div>
-                      </div>
-                      <div className="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
-                        <div className="h-64 w-44 overflow-hidden rounded-lg">
-                          <img src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-06.jpg" alt="" className="h-full w-full object-cover object-center" />
-                        </div>
-                        <div className="h-64 w-44 overflow-hidden rounded-lg">
-                          <img src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-07.jpg" alt="" className="h-full w-full object-cover object-center" />
-                        </div>
-                      </div>
-                    </div>
                   </div>
                 </div>
-
                 <Link href="/contact" className="inline-block rounded-md border border-transparent bg-red-600 py-3 px-8 text-center font-medium text-white hover:bg-red-500">Get in touch</Link>
               </div>
             </div>
