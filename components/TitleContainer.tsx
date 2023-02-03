730d90663dc18a4e271edd26bdbe87b46105dd44
Ronan <roleary81@gmail.com>
2023-02-03 01:35:32 +0100

    enough

--- components/TitleContainer.tsx
+++ components/TitleContainer.tsx
@@ -8,7 +8,7 @@ interface ITitle {
 
 export const TitleContainer = ({ title, subtitle }:ITitle) => {
 
-    const { word } = useTypingText(['CODE...', 'APPS...', 'TECH...', 'DISCOVERY...', 'PUPPIES...', 'MATÉ...'], 250, 20);
+    const { word } = useTypingText(['CODE...', 'APPS...', 'TECH...', 'MEDIA...', 'DISCOVERY...', 'PUPPIES...', 'MATÉ...'], 250, 20);
     
     return (
         <div className="title_container">
