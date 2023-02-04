489ad8714946670f288bc6a38c761ca175b8a7b3
Ronan <roleary81@gmail.com>
2023-02-04 15:22:22 +0100

    add modified curosr styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,21 @@
+import React from "react";
+import dynamic from 'next/dynamic'
 import type { AppProps, NextWebVitalsMetric } from 'next/app';
 import { AnimatePresence, motion } from 'framer-motion';
 import Loader from '../components/Loader';
 import { Header } from '../components/Header';
-import Cursor from '../components/Cursor';
+
 import { useState, useEffect } from 'react';
 import NProgress from 'nprogress';
 import Router from 'next/router'
 import Link from "next/link";
 import { DefaultSeo } from 'next-seo';
-import '../styles/globals.css'
+import '../styles/globals.css';
+
+const AnimatedCursor = dynamic(() => import('react-animated-cursor'), {
+  ssr: false
+});
+
 declare const window: any
 
 // @ts-ignore
@@ -87,7 +94,35 @@ export default function App({ Component, pageProps }) {
         > 
         <>
         {isLoading && <Loader /> } 
-        <Cursor />
+        
+        <AnimatedCursor
+          //@ts-ignore
+          innerSize={8}
+          outerSize={8}
+          color='220, 90, 90'
+          outerAlpha={0.2}
+          innerScale={0.7}
+          outerScale={5}
+          clickables={[
+            'a',
+            'input[type="text"]',
+            'input[type="email"]',
+            'input[type="number"]',
+            'input[type="submit"]',
+            'input[type="image"]',
+            'label[for]',
+            'select',
+            'textarea',
+            'button',
+            '.link'
+          ]}
+          innerStyle={{
+            borderRadius: '0'
+          }}
+          outerStyle={{
+            borderRadius: '0'
+          }}
+        />
         <Component {...pageProps} />
         </>
         </AnimatePresence>
