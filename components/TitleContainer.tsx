102e845e02a691e98a1033b4a056112bc124e770
Ronan <roleary81@gmail.com>
2023-02-13 08:21:07 +0100

    remove wave emoji

--- components/TitleContainer.tsx
+++ components/TitleContainer.tsx
@@ -14,7 +14,7 @@ export const TitleContainer = ({ title, subtitle }:ITitle) => {
         <div className="title_container">
             <div className="grid grid-cols-1 justify-items-center gap-10 homeAdjust mb-6">
                 <div className="mx-auto w-full text-start mt-4">
-                    <h1 className="px-6 font-black text-4xl md:text-6xl mb-6 last:mb-0 uppercase">HELLO THERE! <span className="shake">&#128075;</span><br/>WELCOME TO BUSY<span style={{ "color": "red" }}>{title}</span>PIXELS<span style={{ "color": "red" }}>.</span><br/>{subtitle} <span style={{ "color": "red" }}>&#9825; {word}</span></h1>
+                    <h1 className="px-6 font-black text-4xl md:text-6xl mb-6 last:mb-0 uppercase">HELLO THERE!<br/>WELCOME TO BUSY<span style={{ "color": "red" }}>{title}</span>PIXELS<span style={{ "color": "red" }}>.</span><br/>{subtitle} <span style={{ "color": "red" }}>&#9825; {word}</span></h1>
                 </div>
             </div>
         </div>
